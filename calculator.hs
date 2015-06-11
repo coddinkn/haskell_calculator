3fc0e74d9fd0caaf9069916bcf5c4d36e9873e1a
Killian Coddington <coddinkn@users.noreply.github.com>
2015-06-11 15:36:45 -0400

    calculator is working, just need to add more functions

--- calculator.hs
+++ calculator.hs
@@ -1,70 +1,58 @@
--- infix calculator
+-- cleaned up, more haskell-y version of the infix calculator
 
 import Data.List
-import Data.Maybe
 import Data.Char
+import Data.Maybe
 
-fac :: Int -> Int
-fac 0 = 1
-fac x = x * fac (x - 1)
+removeSpaces :: String -> String
+removeSpaces = filter (/= ' ')
 
 isPerend :: Char -> Bool
 isPerend c = c == '(' || c == ')'
 
-countPerends = length . filter isPerend
+numPerends :: String -> Int
+numPerends = length . filter isPerend
 
-getFirstPerends :: String -> String
-getFirstPerends str = if isNothing n
-						then error "no bracket enclosed expression present"
-						else getFirstPerends' str (fromJust n) ""
-					where n = elemIndex ')' str 
+firstPerends :: String -> String
+firstPerends str = if isNothing i
+	then error "improperly perenthesized expression"
+	else firstPerends' str (fromJust i) ""
+	where i = elemIndex ')' str
 
-getFirstPerends' :: String -> Int -> String -> String
-getFirstPerends' str n list = if (str!!n) /= '('
-								then getFirstPerends' str (n - 1) ((str!!n):list)
-								else ('(':list)
+firstPerends' :: String -> Int -> String -> String
+firstPerends' str i acc = if (str!!i) /= '('
+	then firstPerends' str (i - 1) ((str!!i):acc)
+	else '(':acc
 
-computeNewNumber :: [String] -> String
-computeNewNumber (x:op:y:ys) = show ((read x :: Int) + (read y :: Int)) 
+breakPerends :: String -> [String]
+breakPerends str = breakPerends' str ((length str) - 2) [] [] 
 
-getNumbers :: String -> [String]
-getNumbers str = [getFirstNumber str, getOperator str, getSecondNumber str]
+breakPerends' :: String -> Int -> String -> [String] -> [String]
+breakPerends' str n sacc acc = if n > 0
+	then if isDigit (str!!n) && isDigit (str!!(n - 1))
+		then breakPerends' str (n - 1) ((str!!n):sacc) acc
+		else breakPerends' str (n - 1) [] (((str!!n):sacc):acc) 
+	else acc
 
-getOperator :: String -> String
-getOperator str = filter (\x -> (x /= '(') && (x /= ')') && not (isDigit x)) str
+stringToInt :: String -> Int
+stringToInt str = read str :: Int
 
-getFirstNumber :: String -> String
-getFirstNumber str = getFirstNumber' str [] 1
-
-getFirstNumber' :: String -> String -> Int -> String
-getFirstNumber' str new n = if isDigit (str!!n)
-					then getFirstNumber' str (new ++ [str!!n]) (n + 1)
-					else new  
-
-getSecondNumber :: String -> String
-getSecondNumber str = getSecondNumber' str [] n
-				where n = ((fromJust (elemIndex ')' str)) - 1) 
-
-getSecondNumber' :: String -> String -> Int -> String
-getSecondNumber' str new n = if isDigit (str!!n)
-					then getSecondNumber' str ((str!!n):new) (n - 1)
-					else new
-
-calcPerends :: String -> String
-calcPerends str = 'a':str
+computeList :: [String] -> String
+computeList (x:op:y:[])
+	| op == "+" = show ((stringToInt x) + (stringToInt y))
 
 replacePerends :: String -> String -> String
-replacePerends str x = if isNothing n
-					then error "no bracket enclosed expression present"
-					else filter (/= 'X') (replacePerends' str x (fromJust n))
-				where n = elemIndex ')' str  
+replacePerends str x = if isNothing i
+	then error "improperly perenthesized expression"
+	else filter (/= 'X') (replacePerends' str x (fromJust i))
+	where i = elemIndex ')' str
 
 replacePerends' :: String -> String -> Int -> String
 replacePerends' str x n = if (str!!n) /= '('
-						then replacePerends' (take n str ++ ['X'] ++ drop (n + 1) str) x (n - 1)
-						else (take n str ++ x ++ drop (n + 1) str)
+	then replacePerends' (take n str ++ "X" ++ drop (n + 1) str) x (n - 1)
+	else (take n str ++ x ++ drop (n + 1) str)
 
 calculate :: String -> String
-calculate str = if countPerends str /= 0
-				then calculate (replacePerends str (calcPerends (getFirstPerends str)))
-				else str
+calculate exp = if numPerends exp /= 0
+	then calculate (replacePerends (removeSpaces exp) (computeList (breakPerends (firstPerends (removeSpaces exp)))))
+	else exp	
