18392a5232a6234658a036ce41c1b5c3d74d11ef
Killian Coddington <coddinkn@users.noreply.github.com>
2015-06-11 10:27:36 -0400

    calculator, including semi-modified rpn and beginning of infix

--- calculator.hs
+++ calculator.hs
@@ -1,6 +1,7 @@
 -- this code is currently learn you a haskell's rpn calculator
 
 import Data.List
+import Data.Maybe
 
 fac :: Float -> Float
 fac 0.0 = 1.0
@@ -17,3 +18,23 @@ computeRPN = head . foldl foldingFunction [] . words
 		foldingFunction (x:xs) "!" = fac x:xs 
 		foldingFunction xs "sum" = [sum xs]
 		foldingFunction xs numberString = read numberString:xs
+
+-- my attempt at an infix calculator starts here
+
+isPerend :: Char -> Bool
+isPerend c = c == '(' || c == ')'
+
+countPerends = length . filter isPerend
+
+getFirstPerends :: String -> String
+getFirstPerends str = if isNothing n
+						then "no bracket enclose expression present"
+						else getFirstPerends' str (fromJust n) ""
+					where n = elemIndex ')' str 
+
+getFirstPerends' :: String -> Int -> String -> String
+getFirstPerends' str n list = if (str!!n) /= '('
+									then getFirstPerends' str (n - 1) ((str!!n):list)
+									else ('(':list)
+
+ 
